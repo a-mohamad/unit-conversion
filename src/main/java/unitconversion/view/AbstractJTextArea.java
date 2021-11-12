992d9fca53f36710e115853379a1fa9b525e2143
Amir Mohamad <amohamad@my.yorku.ca>
2021-11-11 21:28:39 -0500

    AbstractJTextArea doesn't implement Subject

--- src/main/java/unitconversion/view/AbstractJTextArea.java
+++ src/main/java/unitconversion/view/AbstractJTextArea.java
@@ -12,12 +12,10 @@ import java.util.List;
 /**
  * The type Abstract j text area.
  */
-public abstract class AbstractJTextArea extends JTextArea implements Subject, Observer {
+public abstract class AbstractJTextArea extends JTextArea implements Observer {
     private static final int WIDTH = 250;
     private static final int HEIGHT = 250;
-
-    private final List<Observer> observers = new LinkedList<>();
-
+    
     /**
      * Instantiates a new Abstract j text area.
      */
@@ -25,20 +23,5 @@ public abstract class AbstractJTextArea extends JTextArea implements Subject, Ob
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setForeground(Color.BLACK);
         insert("0", 0);
-    }
-
-    @Override
-    public void addSubscriber(Observer observer) {
-        observers.add(observer);
-    }
-
-    @Override
-    public void removeSubscriber(Observer observer) {
-        observers.remove(observer);
-    }
-
-    @Override
-    public void notifySubscribers(ValueToConvert valueToConvert) {
-        observers.forEach(o -> o.update(valueToConvert));
     }
 }
