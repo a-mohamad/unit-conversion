07aa7a8b8659f559e7332974bbc8019115805856
Aman <aman.s.chhina@gmail.com>
2021-11-11 22:25:03 -0500

    Expanded on the java docs.

--- src/main/java/unitconversion/model/ValueToConvert.java
+++ src/main/java/unitconversion/model/ValueToConvert.java
@@ -4,7 +4,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * The type Value to convert.
+ * The Value to Convert class takes a dependent and converts it from the dependents type to corresponding
+ * types of every other dependent. The precondition is that the provided value of the dependent is valid
+ * and as such is a numeric. The post condition is that the other dependents have the correct conversion
+ * from the first base dependent type.
  */
 public class ValueToConvert implements Subject {
     private final List<Observer> observers = new LinkedList<>();
@@ -12,11 +15,12 @@ public class ValueToConvert implements Subject {
     private double value;
 
     /**
-     * Sets measurement.
+     * Sets measurement of the provided type to the provided string value converted to its corresponding
+     * numeric value.
      *
-     * @param value the value
-     * @param type  the type
-     * @throws NumberFormatException the number format exception
+     * @param value is the value that will be converted to an integer and used as the updated value.
+     * @param type  is the type of the measurement to be updated.
+     * @throws NumberFormatException the number format exception.
      */
     public void setMeasurement(String value, MeasurementType type) throws NumberFormatException {
         double val;
@@ -30,33 +34,39 @@ public class ValueToConvert implements Subject {
     }
 
     /**
-     * Gets type.
+     * Gets the specific type of measurement.
      *
-     * @return the type
+     * @return the specific type of measurement.
      */
     public MeasurementType getType() {
         return type;
     }
 
     /**
-     * Gets value.
+     * Gets the numeric value that is currently set.
      *
-     * @return the value
+     * @return the numeric value that is currently used.
      */
     public double getValue() {
         return value;
     }
 
+    /**
+     * Adds a subscriber to the Observer pattern as a dependent of which the state should be updated provided a
+     * change and of which the state should be tracked by the other dependents.
+     *
+     * @param observer the observer that is used to add the subscriber.
+     */
     @Override
     public void addSubscriber(Observer observer) {
         observers.add(observer);
     }
 
-    @Override
-    public void removeSubscriber(Observer observer) {
-        observers.remove(observer);
-    }
-
+    /**
+     * Notifies the other dependents that a state change has occurred and to then update their states correspondingly.
+     *
+     * @param valueToConvert the value to convert is the state that is changed and must be
+     */
     @Override
     public void notifySubscribers(ValueToConvert valueToConvert) {
         observers.forEach(o -> o.update(valueToConvert));
