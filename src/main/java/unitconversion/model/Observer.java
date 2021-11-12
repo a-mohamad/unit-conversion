2b05f962e766df0b20b4e1eafdbc85c5712dcb23
Aman <aman.s.chhina@gmail.com>
2021-11-11 22:03:28 -0500

    Added details in regards of the dependents.

--- src/main/java/unitconversion/model/Observer.java
+++ src/main/java/unitconversion/model/Observer.java
@@ -1,13 +1,14 @@
 package unitconversion.model;
 
 /**
- * The interface Observer.
+ * The interface Observer which is used to give the template for our Observer pattern.
  */
 public interface Observer {
     /**
-     * Update.
+     * Updates the dependents of the observer pattern to the provided value to convert.
      *
-     * @param valueToConvert the value to convert
+     * @param valueToConvert the value to convert passed to our converted and allows the dependants in
+     *                       the observer pattern to be updated to.
      */
     void update(ValueToConvert valueToConvert);
 }
