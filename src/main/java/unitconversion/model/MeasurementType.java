21d6b9078054082213b7cbe4916bdeca7571a032
Aman <aman.s.chhina@gmail.com>
2021-11-11 22:01:19 -0500

    Added additional information in regards to the specifics of each measurement.

--- src/main/java/unitconversion/model/MeasurementType.java
+++ src/main/java/unitconversion/model/MeasurementType.java
@@ -1,34 +1,39 @@
 package unitconversion.model;
 
 /**
- * The enum Measurement type.
+ * The enum Measurement type which helps us organize the different type of measurements we have.
  */
 public enum MeasurementType {
     /**
-     * Centimeter measurement type.
+     * Centimeter measurement type used to measure centimeters.
      */
     CENTIMETER("cm"),
     /**
-     * Feet measurement type.
+     * Feet measurement type used to measure feet.
      */
     FEET("ft"),
     /**
-     * Meter measurement type.
+     * Meter measurement type used to measure meters.
      */
     METER("m");
     /**
-     * The Suffix.
+     * The Suffix for each measurement type.
      */
     String suffix;
 
+    /**
+     * When instantiated this adds the corresponding label for the corresponding measurement type.
+     *
+     * @param suffix the label for each of the different measurement types.
+     */
     MeasurementType(String suffix) {
         this.suffix = suffix;
     }
 
     /**
-     * Suffix string.
+     * Suffix string of the measurement type.
      *
-     * @return the string
+     * @return the string of the suffix of the measurement type.
      */
     public String suffix() {
         return suffix;
